c8829920882077b040cc5d6e0dfec489791485f0
Professor-Rafael <101332916+professor-rafael@users.noreply.github.com>
2023-08-20 20:46:40 -0300

    adicionando css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+
+    createCanvas(600, 400);
+    
+    background(" white");
+    
+    }
+    
+    function draw() {
+    noStroke();
+      
+    fill(" darkgreen");
+    rect(0, 0, 200, 600);
+    fill("white");
+    rect(200,0, 200, 600);
+    fill(" red");
+    rect(400,0, 200, 600)
+    }
